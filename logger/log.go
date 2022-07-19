4373537ff24cd4a151f513797230baab4022b358
Jan Dubsky <jan.dubsky@showmax.com>
2022-07-19 12:10:23 +0200

    Add DefeaultLog as old versions of Go did not provide log.Default()

--- logger/log.go
+++ logger/log.go
@@ -4,9 +4,6 @@ import "log"
 
 var _ Logger = &Log{}
 
-// defaultLog is singleton representing default logger of Go log package.
-var defaultLog = NewLog(log.Default())
-
 // Log is Logger using standard logger provided by Go standard log package.
 type Log struct {
 	l *log.Logger
@@ -15,14 +12,31 @@ type Log struct {
 // NewLog creates new Log using l for logging.
 func NewLog(l *log.Logger) *Log { return &Log{l: l} }
 
-// NewDefaultLog returns instance of Log using default logger of Go standard log
-// package.
-func NewDefaultLog() *Log { return defaultLog }
-
 func (l *Log) Errorf(format string, args ...interface{}) {
 	l.l.Printf("error: "+format, args...)
 }
 
 func (l *Log) Warnf(format string, args ...interface{}) {
 	l.l.Printf("warning: "+format, args...)
+}
+
+// Log is Logger using default standard logger provided by Go standard log
+// package.
+type DefaultLog struct {
+	l *log.Logger
+}
+
+// defaultLog is singleton representing default logger of Go log package.
+var defaultLog = &DefaultLog{}
+
+// NewDefaultLog returns instance of Log using default logger of Go standard log
+// package.
+func NewDefaultLog() *DefaultLog { return defaultLog }
+
+func (*DefaultLog) Errorf(format string, args ...interface{}) {
+	log.Printf("error: "+format, args...)
+}
+
+func (*DefaultLog) Warnf(format string, args ...interface{}) {
+	log.Printf("warning: "+format, args...)
 }
