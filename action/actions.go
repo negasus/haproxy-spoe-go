35eedbcd6ba40761c25bfffaf69a02b32ef7f547
Jan Dubsky <jan.dubsky@showmax.com>
2022-07-28 21:15:09 +0200

    [action] Change actions to a plain array

--- action/actions.go
+++ action/actions.go
@@ -1,27 +1,23 @@
 package action
 
-type Actions []*Action
+type Actions []Action
 
 func (actions *Actions) SetVar(scope Scope, name string, value interface{}) {
-	a := AcquireAction()
+	a := Action{}
 	a.SetVar(scope, name, value)
 	*actions = append(*actions, a)
 }
 
 func (actions *Actions) UnsetVar(scope Scope, name string) {
-	a := AcquireAction()
+	a := Action{}
 	a.UnsetVar(scope, name)
 	*actions = append(*actions, a)
 }
 
 func (actions *Actions) Reset() {
-	for _, action := range *actions {
-		ReleaseAction(action)
-	}
-
 	*actions = (*actions)[:0]
 }
 
-func NewActions() *Actions {
-	return &Actions{}
+func NewActions() Actions {
+	return Actions{}
 }
