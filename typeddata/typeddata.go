621c748bb195430dde0172df399f090f2283052b
Brendan Forster <github@brendanforster.com>
2025-03-07 12:58:46 -0400

    bugfix: `false` value has incorrect encoding value - invert flag and value

--- typeddata/typeddata.go
+++ typeddata/typeddata.go
@@ -51,7 +51,7 @@ func Encode(data interface{}, buf []byte) ([]byte, int, error) {
 	case bool:
 		var b byte = 0x11
 		if !v {
-			b = 0x10
+			b = 0x01
 		}
 		buf = append(buf, b)
 		return buf, 1, nil
